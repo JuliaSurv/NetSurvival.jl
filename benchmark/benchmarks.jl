f7ed4e54721cd1732dae993f95fba72327d69499
Oskar Laverny <oskar.laverny@univ-amu.fr>
2024-04-04 19:00:24 +0200

    Add a small benchmark

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,7 +1,26 @@
 using NetSurvival
 using BenchmarkTools
 
-SUITE = BenchmarkGroup()
-SUITE["rand"] = @benchmarkable rand(10)
 
-# Write your benchmarks here.
+# example here : https://github.com/JuliaCI/PkgBenchmark.jl/blob/master/benchmark/benchmarks.jl
+
+suite = BenchmarkGroup()
+# SUITE["rand"] = @benchmarkable rand(10)
+
+
+suite["PoharPerme"] = BenchmarkGroup()
+suite["PoharPerme"]["colrec x frpop - formula"] = @benchmarkable fit(PoharPerme, @formula(Surv(time,status)~sex), colrec, frpop)
+suite["PoharPerme"]["colrec x slopop - explicit"] = @benchmarkable PoharPerme(colrec.time, colrec.status, colrec.age, colrec.year, colrec.sex, slopop)
+
+# Next, the benchmark with hmd rates: 
+# colrec.country = :fracnp
+# SUITE["PoharPerme"]["colrec x hmd_rates"] = @benchmarkable fit(PoharPerme, @formula(Surv(time,status)~sex), colrec, hmd_rates)
+
+# Then, a benchmark for GraffeoTest: 
+suite["GraffeoTest"] = BenchmarkGroup()
+suite["GraffeoTest"]["colrec x slopop - explicit"] = @benchmarkable GraffeoTest(colrec.time, colrec.status, colrec.age, colrec.year, colrec.sex, colrec.sex, colrec.stage, slopop)
+
+tune!(suite)
+results = run(suite, verbose = true)
+
+BenchmarkTools.save("output.json", median(results))
