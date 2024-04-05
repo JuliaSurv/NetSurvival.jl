ee0ce1587ed759c64a8d2ff7e674924347714516
rimhajal <113822034+rimhajal@users.noreply.github.com>
2024-04-05 15:20:08 +0200

    Add test with respect to R code (#3)

--- test/sampletest.jl
+++ test/sampletest.jl
@@ -21,5 +21,40 @@ end
     @test true
 
     # How to use R and R packages in here ? 
+end
+
+
+@testitem "trying RCall" begin
+    
+    # R version
+    using RCall
+    R"""
+    rez = relsurv::rs.surv(
+        survival::Surv(time, stat) ~1, 
+        rmap=list(age = age, sex = sex, year = diag), 
+        data = relsurv::colrec, 
+        ratetable = relsurv::slopop, 
+        method = "pohar-perme", 
+        add.times=1:8149)
+    """
+    R_model = @rget rez
+    R_grid = R_model[:time]
+    R_Sₑ = R_model[:surv]
+    R_σ = R_model[:std_err]
+    R_low = R_model[:lower]
+    R_upp = R_model[:upper]
+
+
+    # Julia version
+    instance = PoharPerme(colrec.time, colrec.status, colrec.age, colrec.year, colrec.sex, slopop)
+    conf_int = confint(instance; level = 0.05)
+    lower_bounds = [lower[1] for lower in conf_int]
+    upper_bounds = [upper[2] for upper in conf_int]
+    
+    err_S = (R_Sₑ .- instance.Sₑ[1:end-1]) ./ R_Sₑ
+    err_σ = (R_σ .- instance.σₑ[1:end-1]) ./ R_σ
+
+    @test all(abs.(err_S) .<= 0.01)
+    @test all(abs.(err_σ) .<= 0.01)
 
 end
