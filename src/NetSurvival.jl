06490d7ee110af70508b574cdf2fcfaa7dfc3d74
Oskar Laverny <oskar.laverny@univ-amu.fr>
2024-05-11 12:15:35 +0200

    add precompile statements

--- src/NetSurvival.jl
+++ src/NetSurvival.jl
@@ -1,5 +1,5 @@
 module NetSurvival
-
+using PrecompileTools: @setup_workload, @compile_workload    # this is a small dependency
 using DataFrames
 using Distributions
 using LinearAlgebra
@@ -31,5 +31,18 @@ export GraffeoTest
 export Surv, Strata
 export colrec
 export @formula
+
+@setup_workload begin
+    # Putting some things in `@setup_workload` instead of `@compile_workload` can reduce the size of the
+    # precompile file and potentially make loading faster.
+    @compile_workload begin
+        fit(PoharPerme, @formula(Surv(time,status)~1), colrec, slopop)
+        fit(PoharPerme, @formula(Surv(time,status)~sex), colrec, slopop)
+        fit(GraffeoTest, @formula(Surv(time,status)~sex), colrec, slopop)
+        fit(GraffeoTest, @formula(Surv(time,status)~sex+Strata(stage)), colrec, slopop)
+        fit(CrudeMortality, @formula(Surv(time,status)~1), colrec, slopop)
+    end
+end
+
 
 end
