7eec1f1d47225e6d10734fa7b3d98bc9e539fdbf
Oskar Laverny <oskar.laverny@univ-amu.fr>
2024-05-11 10:33:17 +0200

    remove useless sample test

--- test/sampletest.jl
+++ test/sampletest.jl
@@ -1,10 +1,3 @@
-# This file could be removed =, but here is a sample test: 
-
-@testitem "trivial test" begin
-    @test true
-end
-
-
 @testitem "trivial test 2" begin
     using RateTables
     ################################
