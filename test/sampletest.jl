b861cc884601fc763c192d59c6c3a45756441393
Oskar Laverny <oskar.laverny@univ-amu.fr>
2024-05-16 22:38:44 +0200

    upgrade test again

--- test/sampletest.jl
+++ test/sampletest.jl
@@ -1,51 +1,47 @@
-@testitem "Compare NPNSEstimator's with relsurv::rs.surv on colrec x slopop" begin
+@testitem "Assess all NPNSEstimators" begin
     using RateTables, NetSurvival, RCall, DataFrames
     function mk_r_model(r_method)
-        @rput r_method
-        R"""
-        rez = relsurv::rs.surv(
-            survival::Surv(time, stat) ~ 1, 
-            rmap=list(age = age, sex = sex, year = diag), 
-            data = relsurv::colrec, 
-            ratetable = relsurv::slopop, 
-            method = r_method, 
-            add.times=1:8149)
-        t = rez$time
-        s = rez$surv
-        e = rez$std.err
-        """
-        t = @rget t
-        s = @rget s
-        e = @rget e
+
         return t::Vector{Float64},s::Vector{Float64},e::Vector{Float64}
     end
     function test_surv(r_method,::Type{E},df, rt, args...) where E
 
-        jl = fit(E, @formula(Surv(time,status)~1), df, rt)
-        ci = confint(npe; level = 0.05)
+        # Main instance: 
+        instance = fit(E, @formula(Surv(time,status)~1), df, rt)
+        ci = confint(instance; level = 0.05)
 
         # Check for no-nans :
-        @test !any(isnan.(npe.Sₑ))
-        @test !any(isnan.(npe.∂Λₑ))
-        @test !any(isnan.(npe.σₑ))
+        @test !any(isnan.(instance.Sₑ))
+        @test !any(isnan.(instance.∂Λₑ))
+        @test !any(isnan.(instance.σₑ))
         @test !any([any(isnan.(x)) for x in ci])
 
         # Check for correspondance of the alternative call syntax: 
-        v2 = E(args..., df, rt)
-        @test all(v2.Sₑ .== npe.Sₑ)
-        @test all(v2.∂Λₑ .== npe.∂Λₑ)
-        @test all(v2.σₑ .== npe.σₑ)
+        v2 = E(args..., rt)
+        @test all(v2.Sₑ .== instance.Sₑ)
+        @test all(v2.∂Λₑ .== instance.∂Λₑ)
+        @test all(v2.σₑ .== instance.σₑ)
 
         # Compare with R: 
-        r_t, r_S, r_σ = mk_r_model(r_method)
-        err_S = zeros(length(r_t))
-        err_σ = zeros(length(r_t))
-        for i in eachindex(r_t) 
-            j = searchsortedlast(jl.grid, r_t[i])
-            err_S[i] = (r_S[i] - jl.Sₑ[j]) / r_S[i]
-            err_σ[i] = (r_σ[i] - jl.σₑ[j]) / r_σ[i]
+        @rput r_method
+        R"""
+        rez = relsurv::rs.surv(survival::Surv(time, stat) ~ 1, rmap=list(age = age, sex = sex, year = diag), data = relsurv::colrec, ratetable = relsurv::slopop, method = r_method, add.times=1:8149)
+        t = rez$time
+        s = rez$surv
+        e = rez$std.err
+        """
+        r = @rget rez
+
+        err_S = zeros(length(r[:time]))
+        err_σ = zeros(length(r[:time]))
+        for i in eachindex(r[:time]) 
+            j = searchsortedlast(instance.grid, r[:time][i])
+            err_S[i] = (r[:surv][i] - instance.Sₑ[j]) / r[:surv][i]
+            err_σ[i] = (r[:std_err][i] - instance.σₑ[j]) / r[:std_err][i]
         end
-        return all(abs.(err_S) .<= 0.01) && all(abs.(err_σ) .<= 0.01)
+        @test all(abs.(err_σ) .<= 0.01)
+        # We return this last one instead of testing it directly because it is broken for Hakulinen, so we can test_broken it only for haku. 
+        return all(abs.(err_S) .<= 0.01) 
     end
     args =  (colrec, slopop, colrec.time, colrec.status, colrec.age, colrec.year, colrec.sex)
     @test        test_surv("pohar-perme", PoharPerme, args...)
@@ -57,7 +53,7 @@
 end
 
 
-@testitem "Comparing log rank test with relsurv::rs.surv on colrec x slopop" begin
+@testitem "Assess GraffeoTest" begin
     
     # R version
     using RCall
@@ -71,10 +67,11 @@ end
     instance = fit(GraffeoTest, @formula(Surv(time,status)~stage), colrec, slopop)
 
     # Check concordance between the two calling syntaxes: 
-    v2 = GraffeoTest(colrec.time, colrec.status, colrec.age, colrec.year, colrec.sex, colrec.sex, colrec.stage, slopop)
+    v2 = GraffeoTest(colrec.time, colrec.status, colrec.age, colrec.year, colrec.sex, ones(length(colrec.age)), colrec.stage, slopop)
     @test all(instance.∂N .== v2.∂N)
     @test all(instance.∂V .== v2.∂V)
     @test all(instance.∂Z .== v2.∂Z)
+
 
     # Check for no nans: 
     @test !any(isnan.(instance.∂N))
@@ -92,6 +89,12 @@ end
     @test !isnan(second_instance.stat)
     @test !isnan(second_instance.df)
     @test !isnan(second_instance.pval)
+
+    # Check concordance between the two calling syntaxes, stratified
+    second_v2 = GraffeoTest(colrec.time, colrec.status, colrec.age, colrec.year, colrec.sex, colrec.sex, colrec.stage, slopop)
+    @test all(second_instance.∂N .== second_v2.∂N)
+    @test all(second_instance.∂V .== second_v2.∂V)
+    @test all(second_instance.∂Z .== second_v2.∂Z)
     
     err_F = (r[:test_stat] - instance.stat) / r[:test_stat]
     err_p = r[:p_value] == 0.0 ? 0.0 : (r[:p_value] - instance.pval) / r[:p_value]
@@ -103,7 +106,7 @@ end
 end
 
 
-@testitem "comparing crude mortality" begin
+@testitem "Assess CrudeMortality" begin
 
     using DataFrames
     using RateTables
