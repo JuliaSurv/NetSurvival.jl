d1dec2bd17d2198f335b306199a74ce294084bd1
Oskar Laverny <oskar.laverny@univ-amu.fr>
2024-12-10 14:46:21 +0100

    up disclaimer message

--- src/fetch_datasets.jl
+++ src/fetch_datasets.jl
@@ -11,7 +11,7 @@ The `colrec` dataset provides an example of relative survival dataset from slove
 - `stage`, cancer stage, can be 1,2,3 or 99 (unknown),
 - `site`, cancer site, can be `:rectum` or `:colon`.
 
-The 5971 patients were diagnosed with colon or rectal cancer in 1994-2000. The original source of the dataset is the [`relsurv` R package](https://CRAN.R-project.org/package=relsurv). Data were provided by the Slovene Cancer Registry, and the `age`,`time` and `year` variables were randomly perturbed to make the identification of patients impossible. Due to this perturbation, no medical conclusion should be made from results extracted from this dataset.
+The 5971 patients were diagnosed with colon or rectal cancer in 1994-2000. The original source of the dataset is the [`relsurv` R package](https://CRAN.R-project.org/package=relsurv). Data were provided by the Slovene Cancer Registry. The original data collected were slightly modified for privacy purpose and legal reason. Because of this data perturbation, neither dates nor ages correspond strictly to their real original values. This dataset is therefore provided as a pedagogical and methodological example and should not be used for medical research purposes.
 
 References: 
 * [Pavlik2018](@cite) Pohar Perme, Maja  and Pavlic, Klemen (2018). Nonparametric relative survival analysis with the R package relsurv. Journal of Statistical Software
@@ -47,7 +47,7 @@ The `ccolon` dataset provides an example of relative survival dataset from franc
 - `stage`, interger from 0 to 3, correspnding to Cancer TNM (tumor node metastatis) stages at diagnosis, either I, II, III, IIIb or IV. 
 - `side`, the primary tumor location, either `:right` or `:left` of the colon.
 
-This dataset has been studied in [Giorgi2003](@cite), [Wolski2020](@cite) and [Laverny2024](@cite). It contains population-based survival data on cases of colorectal cancer from the Registry of Digestive Cancers in Burgundy, France, diagnosed between 1976 and 1990. The `year` variable was randomly perturbed, and patients were censored 1) after 10 years of followup and 2) on the 31 December 1994 based on this perturbed year information, for anonymity purposes. Hence, no medical conclusion should be made from results extracted from this dataset.
+This dataset has been studied in [Giorgi2003](@cite), [Wolski2020](@cite) and [Laverny2024](@cite). It contains population-based survival data on cases of colorectal cancer from the Registry of Digestive Cancers in Burgundy, France, diagnosed between 1976 and 1990. Patients were censored 1) after 10 years of followup and 2) on the 31 December 1994. The original data collected were slightly modified for privacy purpose and legal reason. Because of this data perturbation, neither dates nor ages correspond strictly to their real original values. This dataset is therefore provided as a pedagogical and methodological example and should not be used for medical research purposes.
 
 References: 
 * [Giorgi2003](@cite) Giorgi, Roch and Abrahamowicz, Michal and Quantin, Catherine and Bolard, Philippe and Esteve, Jacques and Gouvernet, Joanny and Faivre, Jean. A relative survival regression model using B-spline functions to model non-proportional hazards. Statistics in medicine
